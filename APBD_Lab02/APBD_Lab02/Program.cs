edd54b4559b5093aecc1b8b6474c45f962d4a4a8
s33324 <s33324@pjwstk.edu.pl>
2026-03-12 13:12:40 +0100

    Zad3: feature-average

--- APBD_Lab02/APBD_Lab02/Program.cs
+++ APBD_Lab02/APBD_Lab02/Program.cs
@@ -2,3 +2,12 @@
 
 Console.WriteLine("Hello, World!");
 
+static int CalculateAverage(int[] values)
+{
+    int sum = 0;
+    for (int i = 0; i < values.Length; i++)
+    {
+        sum += values[i];
+    }
+    return sum / values.Length;
+}
